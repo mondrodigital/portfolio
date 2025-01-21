f7c6a2d52b8405ad2af90636aaa6663abf810928
Mitch's Guest <mitchsguest@MacBookPro.lan>
2025-01-21 04:21:44 -0500

    Fix asset loading

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/',
+  base: '',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
@@ -12,7 +12,10 @@ export default defineConfig({
     minify: 'esbuild',
     rollupOptions: {
       output: {
-        manualChunks: undefined
+        manualChunks: undefined,
+        assetFileNames: 'assets/[name].[ext]',
+        chunkFileNames: 'assets/[name].js',
+        entryFileNames: 'assets/[name].js'
       }
     }
   },
