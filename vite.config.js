4a7373b2db7e209fe1d0b9b6066f6fffe59097ca
Mitch's Guest <mitchsguest@MacBookPro.lan>
2025-01-21 00:28:12 -0500

    V4

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,17 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react({
+      jsxRuntime: 'automatic',
+      jsxImportSource: 'react',
+      babel: {
+        plugins: [
+          ['@babel/plugin-transform-react-jsx', { runtime: 'automatic' }]
+        ]
+      }
+    })
+  ],
   build: {
     rollupOptions: {
       output: {
@@ -12,7 +22,14 @@ export default defineConfig({
         chunkFileNames: 'chunks/[name]-[hash].js',
         assetFileNames: 'assets/[name]-[hash][extname]'
       }
-    }
+    },
+    sourcemap: true
+  },
+  esbuild: {
+    jsxInject: `import React from 'react'`,
+    loader: 'jsx',
+    include: /src\/.*\.jsx?$/,
+    exclude: []
   },
   server: {
     headers: {
